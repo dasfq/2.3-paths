37d878be157a786779cc3bdee9e911935808ab0d
Dasfq <denis-966@mail.ru>
2018-12-19 14:06:32 +0500

    task_3 added
    Moved work# 2.1. File_opening_and_reading
    2.1. File_opening_and_reading:
     - added recepies.txt
     - no global variables.

--- Diplom PY-20/diplom.py
+++ Diplom PY-20/diplom.py
@@ -1,8 +1,17 @@
+# 1. Найти словарь всех групп ЮЗЕРА (id: кол-во участников)
+# 2. Найти список всех друзей Юзера
+# 3. Берём первую группу и первых 500 друзей, проверяем методом group.isMember есть ли среди этих друзей состоящие в данной группе.
+# 4. Если находим хоть одного друга в группе, то удаляем этот id группы из словаря.
+# 5. Перебираем остальные группы
+# 6. Выводим словарь оставшихся групп.
+
+
 import requests, pprint, json, time
 
 class User:
     def __init__(self, token):
         self.token = token
+
     # делаем ссылку для подтверждения прав доступа к нужным данным
     def get_link(app_id):
         app_id = 6788744
@@ -12,21 +21,19 @@ class User:
             'display':'page',
             'scope':'friends,groups',
             'response_type':'token',
-            'v':'5.52'
+            'v':'5.92'
         }
         r = requests.get(auth_url, auth_data)
         print('Линк для получения прав:\n', r.url )
-        return r
+        return
 
     #делаем метод, который будет передавать запрос
     def send_request(self):
         r = requests.get(self.request_url + self.method, self.auth_data).json()
         return r
-    # метод, в котором сначала формируем словарь группа:кол-ва участников, потом удаляем из неё группы, в которых есть друзья
-    def get_groups(self):
-    # returns json file with list of groups without any user's friends
 
-        # get a list of ALL user's groups:
+    def get_groups(self):
+        # get a dict of ALL user's groups:
         self.request_url = 'https://api.vk.com/method/'
         self.method = 'groups.get'
         self.auth_data = {
@@ -34,52 +41,45 @@ class User:
             'extended':1,
             'fields':'members_count',
             'access_token': self.token,
-            'v':'5.52'
+            'v':'5.92'
         }
 
         r = self.send_request()
-        # r = requests.get(request_url+method, auth_data).json()
         groups_dict = {i['id']: i['members_count'] for i in r['response']['items']}
-        print('Список групп:',groups_dict)
+        print('Список всех групп:',groups_dict)
 
         # get a list of user's friends:
         self.method = 'friends.get'
         self.auth_data = {
             'user_id': 171691064,
             'access_token': self.token,
-            'v': '5.52'
+            'v': '5.92'
         }
-
         r = self.send_request()
-        # r = requests.get(request_url+method, auth_data).json()
         friends_list = [i for i in r['response']['items']]
 
         # will delete groups with user's friends from the groups_list
-        self.method = 'groups.isMember'
-        for group_id, members_count in groups_dict.items():
-            for friend in friends_list:
+        for group_id, members_count in groups_dict.copy().items():
+            temp_list = friends_list.copy()
+            while len(temp_list) > 0:
+                friends_list_500 = [temp_list.pop() for i in temp_list[:500] if len(temp_list)>0]
+                # print('list of 500 friends:\n', friends_list_500)
                 self.auth_data = {
                     'group_id': group_id,
-                    'user_id': friend,
+                    'user_ids': friends_list_500,
                     'access_token': self.token,
+                    'extended': '1',
                     'v': '5.52'
                 }
-                try:
-                    r = self.send_request()
-                    # r = requests.get(request_url+method,auth_data).json()
-                    print('r равно:',r)
-                    if r['response'] == 1:
-                        groups_dict.pop(group_id)
-                        break
-                except KeyError:
-                    # Make a pause 2 second due to error "too many requests per second"
-                    time.sleep(2)
-                    r = self.send_request()
-                except RuntimeError:
-                    #
-                    r = self.send_request()
+                self.method = 'groups.isMember'
+                r = self.send_request()
+                time.sleep(0.2)
+                print('r равно:',r)
+            # if r['response'] == 1:
+            #     groups_dict.pop(group_id)
+            #     break
 
-user1 = User(token = '6ec12f01485eaed78e2cdcf91d696e6531b7acb3976177e3132bb72fa469367a46333c59d4dd3d760cbde')
+user1 = User(token = '2ac36695da521d2f3b8498bfe806a24735a1d7ce19f1b2d19ff35ccc2ece3e4836ea8ab7ad5a1a8e8577f')
 print(user1.get_groups())
 
 
---
+++ Diplom PY-20/errors.py
@@ -0,0 +1,2 @@
+{'error': {'error_code': 5, 'error_msg': 'User authorization failed: access_token was given to another ip address.',
+
